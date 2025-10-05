c76a95913d5e688aa4decb6e90e763f27bbdaf44
Sita Vemuri <sitavemuri8@gmail.com>
2025-10-05 11:54:02 -0400

    Collect map coords and send to backend

--- app/api/index.py
+++ app/api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, requests, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,14 @@ CORS(app)
 @app.route('/api/test', methods=['GET'])
 def hello_world():
     return "Hello, World!"
+
+@app.route('/api/save', methods=['POST'])
+def save_message():
+    data = requests.get_json()
+    return jsonify({'status': 'ok', 'data': data})
+
+if __name__ == '__main__':
+    app.run(port=5328)
 
 if __name__ == '__main__':
     app.run(port=5328)
--- app/components/DataFetcher.js
+++ app/components/DataFetcher.js
@@ -2,8 +2,6 @@
 
 import { useEffect, useState } from "react";
 
-console.log('API URL:', process.env.NEXT_PUBLIC_API_URL);
-
 export default function DataFetcher({path}) {
   const [message, setMessage] = useState("");
 
@@ -13,5 +11,5 @@ export default function DataFetcher({path}) {
       .then((data) => setMessage(data));
   }, []);
 
-  return <div>{message}</div>;
+  return <p>{message}</p>;
 }
